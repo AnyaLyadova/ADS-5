f38fc1a25d7184634ce938671690a5ffaee35e5e
AnyaLyadova <124922006+AnyaLyadova@users.noreply.github.com>
2023-04-09 11:54:32 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,32 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+  T arr[size];
+  int top;
+public:
+  TStack() :top(-1) {};
+  ~TStack() {};
+  void Push(const T& value) {
+    arr[++top] = value;
+  }
+  const T& Pop() {
+    return arr[top--];
+  }
+  bool isEmpty()const {
+    return top == -1;
+  }
+  bool isFull() const {
+    return top == size;
+  }
+  const T& Get() const {
+    return top;
+  }
+  T WatchTop() {
+    return arr[top];
+  }
+  void IncTop() {
+    --top;
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
